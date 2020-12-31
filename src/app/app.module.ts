414f215a0a0ea48f0f690e84d7d04fa1647d5631
talshbt <talshbt@gmail.com>
2020-12-31 12:43:35 +0200

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CardComponent } from './card/card.component';
+import { TopRowComponent } from './top-row/top-row.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CardComponent ],
+  declarations: [ AppComponent, HelloComponent, CardComponent, TopRowComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/top-row/top-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-row',
+  templateUrl: './top-row.component.html',
+  styleUrls: ['./top-row.component.css']
+})
+export class TopRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
