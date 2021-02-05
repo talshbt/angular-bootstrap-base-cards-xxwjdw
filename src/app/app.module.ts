c0fc67f1fe8688fc58262f17d6a1e1a2cc3f4fce
talshbt <talshbt@gmail.com>
2021-02-05 10:38:47 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HelloComponent } from './hello.component';
 import { CardComponent } from './card/card.component';
 import { TopRowComponent } from './top-row/top-row.component';
 import { ChildComponent } from './child/child.component';
+import { HorizonaScorllComponent } from './horizona-scorll/horizona-scorll.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CardComponent, TopRowComponent, ChildComponent ],
+  declarations: [ AppComponent, HelloComponent, CardComponent, TopRowComponent, ChildComponent, HorizonaScorllComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/horizona-scorll/horizona-scorll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizona-scorll',
+  templateUrl: './horizona-scorll.component.html',
+  styleUrls: ['./horizona-scorll.component.css']
+})
+export class HorizonaScorllComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
