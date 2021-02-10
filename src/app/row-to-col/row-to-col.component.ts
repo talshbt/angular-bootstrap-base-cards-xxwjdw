04a15dfd1fb5b48701affc36dee30d8fb8db9334
talshbt <talshbt@gmail.com>
2021-02-10 19:33:48 +0200

    2 files modified, 1 added, 1 deleted

--- src/app/row-to-col/row-to-col.component.ts
+++ src/app/row-to-col/row-to-col.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-row-to-col',
   templateUrl: './row-to-col.component.html',
-  styleUrls: ['./row-to-col.component.css']
+  styleUrls: ['./row-to-col.component.scss']
 })
 export class RowToColComponent implements OnInit {
 
