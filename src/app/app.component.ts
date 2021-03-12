8d256ca3c51df986eed44b6f9fa3e8e641062d87
talshbt <talshbt@gmail.com>
2021-03-12 13:22:04 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  msg;
-  name = "Angular";
-  getHistoryData(msg) {
-    alert("xxx");
-    this.msg = msg;
-  }
+export class AppComponent implements OnInit {
+    form: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            acceptTerms: [false, Validators.requiredTrue]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        // display form values on success
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.form.value, null, 4));
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.form.reset();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { CardComponent } from './card/card.component';
-import { TopRowComponent } from './top-row/top-row.component';
-import { ChildComponent } from './child/child.component';
-import { HorizonaScorllComponent } from './horizona-scorll/horizona-scorll.component';
-import { RowToColComponent } from './row-to-col/row-to-col.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CardComponent, TopRowComponent, ChildComponent, HorizonaScorllComponent, RowToColComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
